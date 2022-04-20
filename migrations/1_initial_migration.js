64b3997716b5c968cd70071830a08a947ade20d4
system32uwu <mateocarriqui7@gmail.com>
2022-04-20 14:05:31 -0300

    Set up basic README and instructions to switch

--- migrations/1_initial_migration.js
+++
@@ -1,8 +0,0 @@
-const utils = require("../utils");
-
-const Migrations = artifacts.require("Migrations");
-
-module.exports = async function (deployer) {
-  await deployer.deploy(Migrations);
-  await utils.setup();
-};
